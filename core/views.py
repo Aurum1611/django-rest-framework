a0250f4bb666ca89d7077eb26cf1620254ab0003
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-05-05 16:42:56 +0530

    override retrieve and create methods

--- core/views.py
+++ core/views.py
@@ -7,14 +7,38 @@ from rest_framework.response import Response
 
 class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
-    
+
     def get_queryset(self):
         active_customers = Customer.objects.filter(active=True)
         return active_customers
-    
+
     def list(self, request, *args, **kwargs):
-        customers = self.get_queryset() # Can directly query objects here, method not needed.
+        # Can directly query objects here, method not needed.
+        customers = self.get_queryset()
         serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    # Function called when a specific object is requested through the endpoint.
+    # Example: `/customers/3/` where 3 is the pk argument which will be used to retrieve the data.
+    def retrieve(self, request, *args, **kwargs):
+        obj = self.get_object()
+        serializer = CustomerSerializer(obj)
+        return Response(serializer.data)
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        
+        customer = Customer.objects.create(
+            name=data['name'], addr=data['addr'],
+            data_sheet_id=data['data_sheet'],
+        )
+        
+        profession = Profession.objects.get(id=data['profession'])
+        customer.profession.add(profession)
+        
+        customer.save()
+        
+        serializer = CustomerSerializer(customer)
         return Response(serializer.data)
 
 

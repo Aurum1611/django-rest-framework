0409f7cea6b609926f47b7f8e4f73980402b2bb6
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-05-30 18:03:26 +0530

    StringRelatedField implemented

--- core/serializers.py
+++ core/serializers.py
@@ -4,11 +4,16 @@ from .models import Customer, Profession, Document, DataSheet
 
 class CustomerSerializer(serializers.ModelSerializer):
     num_professions = serializers.SerializerMethodField()
+    data_sheet = serializers.StringRelatedField()   # calls __str__ on data_sheet obj and shows result
+    # many=True for any field that can have multiple values
+    document_set = serializers.StringRelatedField(many=True)    # document has customer fk so the relation
+    profession = serializers.StringRelatedField(many=True)
+    
     
     class Meta:
         model = Customer
         fields = ('id', 'name', 'addr', 'profession_titles', 'data_sheet',
-                  'active', 'num_professions')
+                  'active', 'num_professions', 'document_set', 'profession',)
     
     def get_num_professions(self, obj):
         return obj.num_profs()
